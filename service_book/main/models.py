14c3e203bdd79e06c6eadbda02e94534df63effc
dili <di.grebenev@gmail.com>
2023-02-23 09:59:53 +0300

    silant

---
+++ service_book/main/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Machines(models.Model):
+    ...
+
+class Maintenance(models.Model):
+    ...
+
+class Complaint(models.Model):
+    ...
+
+class ServiceCompany(models.Model):
+    ...
+
+class HDBK(models.Model):
+    ...
