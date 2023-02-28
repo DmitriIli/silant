c7f28763f6798bfd0357463cae66105e0ce5858e
dili <di.grebenev@gmail.com>
2023-02-28 06:08:23 +0300

    add models

--- service_book/main/models.py
+++ service_book/main/models.py
@@ -10,13 +10,36 @@ def getOperatingTime():
 
 class Parts(models.Model):
     name = models.CharField(
-        primary_key=True, max_length=2, blank=False, unique=True)
+        primary_key=True, blank=False, null=False, unique=True)
+
+
+class ModelMachine(models.Model):
+    name = models.CharField(
+        primary_key=True, blank=False, null=False, unique=True)
     description = models.CharField(default='описание')
 
 
-class RepairMetod(models.Model):
-    name = models.CharField(primary_key=True, max_length=256,
-                            blank=False, null=False, unique=True)
+class Engine(models.Model):
+    name = models.CharField(
+        primary_key=True, blank=False, null=False, unique=True)
+    description = models.CharField(default='описание')
+
+
+class Transmission(models.Model):
+    name = models.CharField(
+        primary_key=True, blank=False, null=False, unique=True)
+    description = models.CharField(default='описание')
+
+
+class DriveAxel(models.Model):
+    name = models.CharField(
+        primary_key=True, blank=False, null=False, unique=True)
+    description = models.CharField(default='описание')
+
+
+class SteringAxel(models.Model):
+    name = models.CharField(
+        primary_key=True, blank=False, null=False, unique=True)
     description = models.CharField(default='описание')
 
 
@@ -32,116 +55,59 @@ class ClientCompany(models.Model):
     description = models.CharField(default='описание')
 
 
-class Catalog(models.Model):
-
-    # THECHNIC_MODEL = 'THM'
-    # ENGINE_MODEL = 'EM'
-    # TRANSMSSION_MODEL = 'TRM'
-    # DRIVE_AXEL_MODEL = 'DAM'
-    # STERING_AXEL_MODEL = 'SAM'
-    # SERVICE_COMPANY = 'SC'
-    # CLIENT_COMPANY = 'CC'
-    # TYPE_OF_MAINTANANCE = 'TM'
-    # FAILURE_TYPE = 'FT'
-    # REPAIR_METOD = 'RM'
-
-    # catalog = [
-    #     THECHNIC_MODEL= 'THM'
-    #     ENGINE_MODEL = 'EM'
-    #     TRANSMSSION_MODEL = 'TRM'
-    #     DRIVE_AXEL_MODEL = 'DAM'
-    #     STERING_AXEL_MODEL = 'SAM'
-    #     SERVICE_COMPANY = 'SC'
-    #     CLIENT_COMPANY = 'CC'
-    #     TYPE_OF_MAINTANANCE = 'TM'
-    #     FAILURE_TYPE = 'FT'
-    #     REPAIR_METOD = 'RM'
-    # ]
-
-    CATALOG = [
-        ('THM', 'Model of technic'),
-        ('EM', 'Engine model'),
-        ('TRM', 'Transmission model'),
-        ('DAM', 'Drive Axel model'),
-        ('SAM', 'Stering Axel model'),
-        ('SC', 'Service Company'),
-        ('CC', 'Client Company'),
-        ('TM', 'Type of Maintanance'),
-        ('FT', 'Failure Type'),
-        ('RM', 'Repaire Metod')
-
-    ]
-
-    subject = models.CharField(
-        max_length=3, choices=CATALOG, blank=False, null=False)
+class TypeOfService(models.Model):
     name = models.CharField(
-        primary_key=True, unique=True, blank=False, null=False)
-    description = models.CharField(default='описание', blank=False, null=False)
+        primary_key=True, blank=False, null=False, unique=True)
+    description = models.CharField(default='описание')
 
 
-class Machines(models.Model):
-    machineFuctoryNumber = models.CharField(
-        unique=True, blank=False, null=False)
-    machineModel = models.ForeignKey(
-        Catalog, to_field='name', on_delete=models.DO_NOTHING)
-    engineFactoryNumber = models.CharField(
-        unique=True, blank=False, null=False)
-    engineModel = models.ForeignKey(
-        Catalog, to_field='name', on_delete=models.DO_NOTHING)
-    transmissionFactoryNumber = models.CharField(
-        unique=True, blank=False, null=False)
-    transmissionModel = models.ForeignKey(
-        Catalog, to_field='name', on_delete=models.DO_NOTHING)
-    driveAxelFactoryNumber = models.CharField(
-        unique=True, blank=False, null=False)
-    driveAxelModel = models.ForeignKey(
-        Catalog, to_field='name', on_delete=models.DO_NOTHING)
-    steringAxelFactoryNumber = models.CharField(
-        unique=True, blank=False, null=False)
-    steringAxelModel = models.ForeignKey(
-        Catalog, to_field='name', on_delete=models.DO_NOTHING)
-    supplyContract = models.CharField(
-        unique=True, blank=False, null=False)
-    shippingDate = models.DateField(
-        default=date.today())
-    destination = models.CharField(blank=False, null=False)
-    options = models.CharField(
-        default='доп. опции отсутствуют')
-    clientCompany = models.ForeignKey(
-        Catalog, to_field='name', on_delete=models.DO_NOTHING)
-    serviceCompany = models.ForeignKey(
-        Catalog, to_field='name', on_delete=models.DO_NOTHING)
+class RecoveryMethode(models.Model):
+    name = models.CharField(
+        primary_key=True, blank=False, null=False, unique=True)
+    description = models.CharField(default='описание')
+
+# repair methods
+
+
+class Machine(models.Model):
+    modelMachine = models.ForeignKey(ModelMachine, to_field='name')
+    factoryNumberMachine = models.CharField(max_length=64, unique=True)
+    engine = models.ForeignKey(Engine, to_field='name')
+    factoryNumberEngene = models.CharField(max_length=64, unique=True)
+    transmission = models.ForeignKey(Transmission, to_field='name')
+    factoryNumberTransmission = models.CharField(max_length=64, unique=True)
+    driveAxel = models.ForeignKey(DriveAxel, to_field='name')
+    factoryNumberDriveAxel = models.CharField(max_length=64, unique=True)
+    steringAxel = models.ForeignKey(SteringAxel, to_field='name')
+    factoryNumberSteringAxel = models.CharField(max_length=64, unique=True)
+    supplyContract = models.CharField(max_length=64)
+    shipingDate = models.DateField(default=date.today())
+    receiver = models.CharField(max_length=64)
+    deliveryAddress = models.CharField(max_length=128)
+    equipment = models.CharField(
+        max_length=128,  default='стандартная комплектация')
+    client = models.ForeignKey(ClientCompany, to_field='name')
+    serviceCompany = models.ForeignKey(ServiceCompany, to_field='name')
+
+
+class Service(models.Model):
+    typeOfService = models.ForeignKey(TypeOfService, to_field='name')
+    dateService = models.DateField(default=date.today())
     operatingTime = models.IntegerField(default=0)
+    order = models.CharField(max_length=64)
+    orderDate = models.DateField(default=date.today())
+    serviceCompany = models.ForeignKey(ServiceCompany, to_field='name')
+    machine = models.ForeignKey(Machine, to_field='name')
 
 
-class Maintenance(models.Model):
-    typeOfMaintance = models.ForeignKey(
-        Catalog, to_field='name', on_delete=models.DO_NOTHING)
-    dateOfMaintenance = models.DateField(
-        default=date.today(), blank=True, null=True)
-    operatingTime = models.IntegerField()
-    numberOfOrder = models.CharField(blank=False, null=False)
-    dateOfOrder = models.DateField(default=date.today(), blank=True, null=True)
-    serviceCompany = models.ForeignKey(
-        Catalog, to_field='name', on_delete=models.DO_NOTHING)
-    machine = models.ForeignKey(
-        Catalog, to_field='name', on_delete=models.DO_NOTHING)
-
-
-class Complaint(models.Model):
-
-    subject = models.ForeignKey(
-        Machines, to_field='name', blank=False, null=False, on_delete=models.DO_NOTHING)
-    serviceCompany = models.ForeignKey(
-        ServiceCompany, to_field='name', blank=False, null=False, on_delete=models.DO_NOTHING)
-    dateOfComplaint = models.DateField(default=date.today())
+class Complainte(models.Model):
+    machine = models.ForeignKey(Machine, to_field='name')
+    failureDate = models.DateField(default=date.today())
     operatingTime = models.IntegerField(default=0)
-    failureNode = models.ForeignKey(
-        Parts, to_field='name', blank=False, null=False)
-    description = models.CharField(default='описание', max_length=256)
-    repairMetod = models.ForeignKey(
-        RepairMetod, to_field='name', blank=False, null=False)
-    repairDetails = models.CharField(
-        default='используемые части', max_length=256)
-    dateComplite = models.DateField(default=date.today())
+    failurePart = models.ForeignKey(Parts, to_field='name')
+    failureDescription = models.CharField(max_length=256, default='описание')
+    recoveryMethod = models.ForeignKey(RecoveryMethode, to_field='name')
+    spareParts = models.CharField(default='зап.части')
+    recoveryDate = models.DateField(default=date.today())
     downTime = models.IntegerField(default=0)
+    serviceCompany = models.ForeignKey(ServiceCompany, to_field='name')
